3042fb0d55aba13e9f678346902a486c023fb358
Marcel Savegnago <marcel.savegnago@gmail.com>
2022-01-13 17:21:53 -0300

    [IMP] mis_builder_analytic: Migration to 12.0

--- mis_builder_analytic/__manifest__.py
+++ mis_builder_analytic/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "MIS Builder Analytic",
     "summary": "Provide account analytic lines for MIS builder reports",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "license": "AGPL-3",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/mis-builder",
