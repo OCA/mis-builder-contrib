07cdd4263c04507a84e5e55926e997fdeae9f4e1
Camille Morand <camille.morand@camptocamp.com>
2022-11-25 11:33:24 +0100

    [15.0][ADD] mis_builder_budget_product: initial commit

---
+++ mis_builder_budget_product/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2022 Camptocamp SA (https://www.camptocamp.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "MIS Builder Budget Contributions",
+    "summary": """
+            Offer more options for budgets for MIS reports""",
+    "version": "15.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/mis-builder-contrib",
+    "depends": ["mis_builder_budget"],
+    "data": [
+        "views/mis_budget_by_account_item.xml",
+    ],
+    "installable": True,
+}
