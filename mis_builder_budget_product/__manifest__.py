9869baebb36cec4823c1d52af34af8b30a7ad6c5
Mussie Sirak <sirak.mussie@camptocamp.com>
2023-06-12 11:23:15 +0200

    [MIG] mis_builder_budget_product : Migration to 16.0

--- mis_builder_budget_product/__manifest__.py
+++ mis_builder_budget_product/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "MIS Builder Budget Contributions",
     "summary": """
             Offer more options for budgets for MIS reports""",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/mis-builder-contrib",
