96387d87fa6d045adac6e46872d147f14ba37bad
Zina Rasoamanana <zina.rasoamanana@acsone.eu>
2025-01-29 16:29:53 +0100

    [ADD] mis_builder_wizard_export_xlsx: init

---
+++ mis_builder_wizard_export_xlsx/models/__init__.py
@@ -0,0 +1,2 @@
+from . import mis_report_instance
+from . import report_mis_builder_mis_report_instance_xlsx
---
+++ mis_builder_wizard_export_xlsx/models/mis_report_instance.py
@@ -0,0 +1,27 @@
+# Copyright 2025 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class MisReportInstance(models.Model):
+
+    _inherit = "mis.report.instance"
+
+    def open_wizard_custom_export_xls(self):
+        return self.env["ir.actions.act_window"]._for_xml_id(
+            "mis_builder_wizard_export_xlsx.mis_builder_custom_export_xls_wizard_act_window"
+        )
+
+    def _compute_pivot_date(self):
+        ret = super()._compute_pivot_date()
+        report_id_by_pivot_date = self.env.context.get(
+            "mis_builder_xls_report_id_by_pivot_date"
+        )
+        if report_id_by_pivot_date:
+            for rec in self:
+                pivot_date = report_id_by_pivot_date.get(str(rec.id))
+                if pivot_date:
+                    rec.pivot_date = pivot_date
+
+        return ret
